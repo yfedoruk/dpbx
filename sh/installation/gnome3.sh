542f5f22e799e91d5cc41b911b6e2748b70c3b98
yfedoruck <yfedoruck@gmail.com>
2015-02-03 19:04:31 +0200

    --

--- sh/installation/gnome3.sh
+++ sh/installation/gnome3.sh
@@ -8,10 +8,13 @@ sudo apt-get -y install gnome-shell
 sudo apt-get -y install gnome-tweak-tool
 
 
-Failed to start session after upgrade to 14.04
-I faced the same problem after the upgrade.. I tried to install Gnome env. and surprisingly this got fixed.
+#Failed to start session after upgrade to 14.04
+#I faced the same problem after the upgrade.. I tried to install Gnome env. and surprisingly this got fixed.
 
 sudo apt-get install gnome
+sudo apt-get install gnome-session-flashback
+sudo apt-get install unity-settings-daemon
 sudo apt-get install gnome-session-fallback
+sudo apt-get install gnome-settings-daemon-schemas=3.8.6.1-0ubuntu11.2
 sudo reboot
 
