eb33d057eaef75feed0e704eaf756fb8e17185da
yfedoruck <yfedoruck@gmail.com>
2015-02-04 16:30:17 +0200

    --

---
+++ sh/installation/CapsLock.Esc.sh
@@ -0,0 +1,6 @@
+#sudo dumpkeys > ~/loadkeys_backup.map
+#sudo loadkeys ~/Dropbox/caps_to_esc.map
+
+#In recent versions of both Debian and Ubuntu you'll need an extra utility:
+#sudo apt-get install gnome-tweak-tool
+#Run this by typing "tweak tool" in to the GNOME launcher or Ubuntu Dash, go to the Typing tab and select "Make Caps Lock an additional ESC" under the Caps Lock key behaviour dropdown.
--- sh/installation/ffmpeg.sh
+++ sh/installation/ffmpeg.sh
@@ -1,3 +1,5 @@
 #12.04 ????? http://ffmpeg.org/download.html
 sudo add-apt-repository ppa:jon-severinsson/ffmpeg
+#14.04
+sudo add-apt-repository ppa:mc3man/trusty-media
 sudo apt-get update && sudo apt-get install ffmpeg
