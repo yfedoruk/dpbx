7ed066feed7e62c53fc653b37c43f021281d1c88
yfedoruck <yfedoruck@gmail.com>
2015-02-27 19:16:57 +0200

    --

--- jQuery/checked.js
+++
@@ -1,6 +0,0 @@
-$('.myCheckbox').prop('checked', true);
-$('.myCheckbox').prop('checked', false);
-
-// jQ < 1.5
-$('.myCheckbox').attr('checked','checked'); 
-$('.myCheckbox').removeAttr('checked');
---
+++ sh/settings/autostart/autostart.sh
@@ -0,0 +1,1 @@
+~/.config/autostart/filename.desctop
