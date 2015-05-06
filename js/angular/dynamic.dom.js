6de45c9083e31689e2df6f4c258464128d6a73a2
yfedoruck <yfedoruck@gmail.com>
2015-05-06 19:43:33 +0300

    --

--- js/angular/dynamic.dom.js
+++ js/angular/dynamic.dom.js
@@ -12,3 +12,41 @@ var addsett = $('.add-settings').detach();
 angular.element($event.currentTarget).parent().append(
 	$compile(addsett)($scope)
 );
+
+// example angular
+
+var dfrd = $.Deferred(),
+	tpl_url = baseurl + '/themes/kangaroo/views/calendar/assets/tpl/add-settings.html',
+	tplcache = $templateCache.get(tpl_url),
+	tpl;
+
+if( typeof tplcache !=='undefined' ){
+	tpl = tplcache[1];
+	dfrd.resolve();
+}else{
+	$http.get(tpl_url, { cache: $templateCache }).success(function(data){
+		tpl = data;
+		dfrd.resolve();
+	});
+}
+dfrd.promise().then(function(){
+	angular.element($event.currentTarget).parent().append(
+		$compile(tpl)($scope)
+	);
+});
+
+
+// example JQuery
+
+var dfrd = $.Deferred();
+var tpl = $.get(baseurl + '/themes/kangaroo/views/calendar/assets/tpl/add-settings.html').done(function(){
+	dfrd.resolve();
+});
+
+dfrd.promise().then(function(){
+	angular.element($event.currentTarget).parent().append(
+		$compile(tpl.responseText)($scope)
+	);
+	$scope.$apply();
+});
+$scope.Calendars.Kangaroo.settings= true;
