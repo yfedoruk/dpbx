40ddbd1ed48f2e83c7e617de91ec37ad201a9f6b
yfedoruck <yfedoruck@gmail.com>
2015-05-27 20:45:59 +0300

    --

--- sh/settings/ppa.sh
+++ sh/settings/ppa.sh
@@ -1,3 +1,17 @@
 #List All Ubuntu PPAs with Single Command http://www.ubuntuvibes.com/2012/10/list-all-ubuntu-ppas-with-single-command.html
 grep -RoPish "ppa.launchpad.net/[^/]+/[^/ ]+" /etc/apt | sort -u | sed -r 's/\.[^/]+\//:/'
 
+#for removing ppa
+sudo apt-get install ppa-purge
+
+#for packages from official repositories
+sudo ppa-purge ppa_name
+
+#from unofficial
+sudo apt-get purge package_name
+
+#universal (not safe)
+sudo add-apt-repository --remove ppa:whatever/ppa
+
+#best
+rm /etc/apt/sources.list.d/{{ppa_name}}
