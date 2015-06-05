7bdc1e527b25da9f67f067da5bcddc4d6a3c56b5
yfedoruck <yfedoruck@gmail.com>
2015-06-05 19:20:40 +0300

    --

---
+++ sh/emacs.base.sh
@@ -0,0 +1,10 @@
+#How do I create an empty file in emacs? (http://stackoverflow.com/questions/2592095/how-do-i-create-an-empty-file-in-emacs)
+
+You can use the touch command:
+
+M-! touch __init__.py RET
+
+#How to open terminal? (http://www.gnu.org/software/emacs/manual/html_node/emacs/Terminal-emulator.html)
+
+M-x term
+
