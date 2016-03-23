741ddf615a672798865911f8950563dc7fbce8e8
yfedoruck <yfedoruck@gmail.com>
2016-03-23 12:16:09 +0200

    --

--- _/apache.sh
+++ _/apache.sh
@@ -1,5 +1,4 @@
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libapache2-mod-php5
-sudo a2enmod rewrite
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libapache2-mod-php5 && a2enmod rewrite && service apache2 restart
 #test
 apache2ctl -M | grep  rewrite_module
-sudo service apache2 restart
+
