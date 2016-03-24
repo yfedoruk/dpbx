d4f25209f55cfcf296f5aeb1fba7e5327df32048
y.fedoruk <y.fedoruk@favbet.com>
2016-03-24 17:24:38 +0200

    --

---
+++ git/git_config.sh
@@ -0,0 +1,30 @@
+# git config --global user.name "yfedoruck"
+# git config --global user.email yfedoruck@gmail.com
+git config --global user.name "y.fedoruk"
+git config --global user.email y.fedoruk@favbet.com
+
+git config --global core.pager 'less -+$LESS -R' #exit from `pager`
+git config --global pager.diff false #or git --no-pager diff
+
+
+git config --global core.mergeoptions --no-edit
+git config --global push.default current
+git config --global pull.default current # must set before: git branch -u origin/branch_name
+
+git config core.filemode false
+git config alias.checkout co
+git config alias.status stat
+git config color.diff.old red bold
+git config color.diff.new green bold
+git config color.diff auto
+git config color.branch.local yellow
+git config color.branch.remote green
+git config color.status.added yellow
+git config color.status.changed green
+git config color.status.untracked cyan
+git config color.diff.old red bold
+git config color.diff.meta yellow bold
+git config color.diff.frag cyan bold
+
+git config --global color.ui true
+git config --global color.ui auto
