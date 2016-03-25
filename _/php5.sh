9ec1ddfb572f179ad0ee5d7b11179498db3c2c48
y.fedoruk <y.fedoruk@favbet.com>
2016-03-25 18:50:59 +0200

    --

--- _/php5.sh
+++ _/php5.sh
@@ -1,2 +1,8 @@
 sudo apt-get install php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-pspell php5-recode php5-sqlite  php5-tidy php5-xmlrpc php5-xsl php5-json php5-pgsql php5-xdebug
 sudo apt-get remove php5-snmp #or sudo apt-get install snmp
+
+
+#settings 
+# $: php --ini
+# /etc/php5/apache2/php.ini
+# short_open_tag = On
