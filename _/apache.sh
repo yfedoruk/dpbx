2e50b8b6c87c0a90e3f827a5c19fffef0b396e8d
yfedoruck <yfedoruck@gmail.com>
2016-09-25 14:52:08 +0300

    --

--- _/apache.sh
+++ _/apache.sh
@@ -1,7 +1,10 @@
-sudo apt-get install apache2 apache2-doc apache2-utils libapache2-mod-php5 && a2enmod rewrite && service apache2 restart
+apt install -y apache2 apache2-doc apache2-utils libapache2-mod-php && a2enmod rewrite && service apache2 restart
 
 #sudo apt-get install apache2-mpm-prefork
-sudo apt-get install libapache2-mod-php7.0
+apt install -y libapache2-mod-php7.0
+
+echo 'ServerName localhost' >> /etc/apache2/apache2.conf
 #test
-apache2ctl -M | grep  rewrite_module
+sudo apt -y autoremove
+apache2ctl -M | grep rewrite_module
 
--- _/php5.sh
+++ _/php5.sh
@@ -1,9 +1,12 @@
 #http://askubuntu.com/questions/565784/how-do-i-upgrade-php-version-to-the-latest-stable-released-version
 
-sudo apt-add-repository -y ppa:ondrej/php5-5.6
-sudo apt-get update
-sudo apt-get install php5.6 php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-idn php-pear php5.6-imagick php5.6-imap php5.6-mcrypt php5.6-memcache php5.6-mhash php5.6-mysql php5.6-pspell php5.6-recode php5.6-sqlite  php5.6-tidy php5.6-xmlrpc php5.6-xsl php5.6-json php5.6-pgsql php5.6-xdebug
+sudo apt-add-repository ppa:ondrej/php5-5.6
+sudo apt update
+sudo apt -y install php5.6 php5.6-common php5.6-curl php5.6-dev php5.6-gd php-pear php-imagick php5.6-imap php5.6-mcrypt php-memcache php5.6-mysql php5.6-pspell php5.6-recode php5.6-sqlite3  php5.6-tidy php5.6-xmlrpc php5.6-xsl php5.6-json php5.6-pgsql php-xdebug
 sudo apt-get remove php5-snmp #or sudo apt-get install snmp
+
+#php5.6-idn
+#php5.6-mhash
 
 #problems php5-5.6
 #php5-ming
@@ -13,7 +16,7 @@ sudo apt-get remove php5-snmp #or sudo apt-get install snmp
 # /etc/php5/apache2/php.ini
 # short_open_tag = On
 
-sed -i 's/short_open_tag = Off/short_open_tag = On/' /etc/php5/apache2/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/' /etc/php/5.6/apache2/php.ini
 
 # xdebug disable in terminal (cli) #http://stackoverflow.com/a/35438287
 # sudo php5dismod -s cli xdebug
