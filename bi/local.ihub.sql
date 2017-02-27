be0bd365e92da71a2c2e6cab2dda129282b978ae
y.fedoruk <y.fedoruk@favbet.com>
2017-02-27 13:58:52 +0200

    --

---
+++ bi/local.ihub.sql
@@ -0,0 +1,3 @@
+/*** local ihub (from outer) ***/ (from docker-compose)
+
+psql -U postgres -h ihub.dev -p 15432 -d ihub -W mysecretpassword
---
+++ sh/sound/increase_volume.sh
@@ -0,0 +1,3 @@
+sudo apt install pulseaudio-utils
+
+# invoke Pulse Audo Volume Control -> Output Devices.
