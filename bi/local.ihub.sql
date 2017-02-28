8ab159fc0c51600527a34175dd08be77dcb1dbf0
y.fedoruk <y.fedoruk@favbet.com>
2017-02-28 13:33:25 +0200

    --

--- bi/local.ihub.sql
+++ bi/local.ihub.sql
@@ -1,3 +1,6 @@
 /*** local ihub (from outer) ***/ (from docker-compose)
 
 psql -U postgres -h ihub.dev -p 15432 -d ihub -W mysecretpassword
+
+/*** local icms (from outer) ***/
+psql -U postgres -h localhost -p 10000 -d ihub -W mysecretpassword
