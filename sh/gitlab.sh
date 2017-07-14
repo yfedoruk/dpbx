a24394a3186417023abc89ec2c5bfdbe6301182d
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2017-07-14 18:23:53 +0300

    --

--- sh/gitlab.sh
+++ sh/gitlab.sh
@@ -14,13 +14,27 @@ else
     exit 1
 fi
 
-echo "Source branch:"
-read source_branch
+source_branch=`git symbolic-ref --short HEAD`
+echo "Source branch is: ${source_branch} ? (y/n)"
+read -s -n 1 is_source_correct
+
+
+if [[ "${is_source_correct}" == "y" || "${is_source_correct}" == "" ]]; then
+	echo "...proceed..."
+elif [ "${is_source_correct}" == "n"  ]; then
+	echo "Enter source branch:"
+    read source_branch
+else
+    echo "wrong!"
+    exit 1
+fi
+
+url=""
 
 echo "Create merge reauest ${source_branch} ==> dev"
-iid=`curl -k -H "PRIVATE-TOKEN: " -X POST "%2F${project}/merge_requests?source_branch=${source_branch}&target_branch=dev&title=${source_branch}&assignee_id=75" | jq '.iid'`
+iid=`curl -k -H "PRIVATE-TOKEN: " -X POST "${url}?source_branch=${source_branch}&target_branch=dev&title=${source_branch}&assignee_id=75" | jq '.iid'`
 
 echo "iid=${iid}"
 
 echo "merge ${source_branch} ==> dev"
-curl -k -H "PRIVATE-TOKEN: " -X PUT "%2F${project}/merge_requests/${iid}/merge?&should_remove_source_branch=false&merge_when_pipeline_succeeds=true"
+curl -k -H "PRIVATE-TOKEN: " -X PUT "${url}/${iid}/merge?&should_remove_source_branch=false&merge_when_pipeline_succeeds=true"
