c9acd4e6523041057faabc11eb8d1845d7820db3
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2017-07-14 18:24:43 +0300

    --

--- git/config.sh
+++ git/config.sh
@@ -11,20 +11,20 @@ git config --global core.mergeoptions --no-edit
 git config --global push.default current
 git config --global pull.default current # must set before: git branch -u origin/branch_name
 
-git config core.filemode false
-git config alias.checkout co
-git config alias.status stat
-git config color.diff.old red bold
-git config color.diff.new green bold
-git config color.diff auto
-git config color.branch.local yellow
-git config color.branch.remote green
-git config color.status.added yellow
-git config color.status.changed green
-git config color.status.untracked cyan
-git config color.diff.old red bold
-git config color.diff.meta yellow bold
-git config color.diff.frag cyan bold
+git config --global core.filemode false
+git config --global alias.checkout co
+git config --global alias.status stat
+git config --global color.diff.old red bold
+git config --global color.diff.new green bold
+git config --global color.diff auto
+git config --global color.branch.local yellow
+git config --global color.branch.remote green
+git config --global color.status.added yellow
+git config --global color.status.changed green
+git config --global color.status.untracked cyan
+git config --global color.diff.old red bold
+git config --global color.diff.meta yellow bold
+git config --global color.diff.frag cyan bold
 
 git config --global color.ui true
 git config --global color.ui auto
