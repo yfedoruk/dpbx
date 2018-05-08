18c17c5c24bab8f474d79fed8534a4ef3900dfda
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2018-05-08 15:16:42 +0300

    --

--- mus/ape-to-flac.sh
+++ mus/ape-to-flac.sh
@@ -1,1 +1,7 @@
+# https://askubuntu.com/questions/756800/batch-convert-and-split-ape-with-cue-to-flac-in-various-folders-from-terminal
+
+# convert solid .ape file to .flac
 find . -name "*.ape" -exec sh -c 'exec ffmpeg -i "$1" "${1%.ape}.flac"' _ {} \;
+
+# then split .flac with .cue (info) to flac tracks
+find . -name "*.cue" -exec sh -c 'exec shnsplit -f "$1" -o flac -t "%n_%p-%t" "${1%.cue}.flac"' _ {} \;
