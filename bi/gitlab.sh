2ce14cbf83308428387452b8b1b2fc7051c3fb21
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2019-07-19 15:02:16 +0300

    --

---
+++ bi/gitlab.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+project=`git config --get remote.origin.url`
+delimiter="%2F"
+source_branch=`git symbolic-ref --short HEAD`
+token=$(<~/bi/gitlab_token.txt)
+
+
+project="${project:19}"
+project="${project:0:-4}"
+echo $project
+
+project="${project//\//$delimiter}"
+
+url="https://gitlab.favorit/api/v4/projects/${project}/merge_requests"
+echo $url
+
+
+echo "Source branch is: ${source_branch} ? (y/n)"
+read -s -n 1 is_source_correct 
+
+if [[ "${is_source_correct}" == "y" || "${is_source_correct}" == "" ]]; then
+    echo "...proceed..."
+elif [[ "${is_source_correct}" == "n"  ]]; then
+    echo "Enter source branch:"
+    read source_branch
+else
+    echo "wrong!"
+    exit 1
+fi
+
+echo "Create merge request ${source_branch} ==> dev"
+
+echo "${url}?source_branch=${source_branch}&target_branch=dev&title=${source_branch}&assignee_id=75"
+#exit 1;
+
+create_result=`curl -k -H "PRIVATE-TOKEN: ${token}" -X POST "${url}?source_branch=${source_branch}&target_branch=dev&title=${source_branch}&assignee_id=75"`
+#create_result="{\"id\":26436,\"iid\":1373dd}"
+
+iid=`echo ${create_result} | jq '.iid'`
+
+#is iid a number?
+re='^[0-9]+$'
+if ! [[ ${iid} =~ $re ]] ; then
+   echo "error: ${create_result}" >&2;
+   exit 1
+fi
+
+echo "iid=${iid}"
+
+echo "merge ${source_branch} ==> dev"
+merge_result=`curl -k -H "PRIVATE-TOKEN: ${token}" -X PUT "${url}/${iid}/merge?&should_remove_source_branch=false&merge_when_pipeline_succeeds=true" | jq '.message'`
+echo ${merge_result}
+message=`echo ${merge_result} | jq '.message'`
+
+#if [[ "${message}" == "405 Method Not Allowed" ]]; then
+#    curl -k -H "PRIVATE-TOKEN: ${token}" -X DELETE "${url}/${iid}"
+#fi
