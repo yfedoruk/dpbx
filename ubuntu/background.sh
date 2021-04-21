87ccae6f9b7bf6038d574cc0f51edd8470218f21
penguin <penguin@syneforge.com>
2021-04-21 14:23:33 +0300

    --

--- ubuntu/background.sh
+++ ubuntu/background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove background picture
-gsettings set org.gnome.desktop.bacgsettings set org.gnome.desktop.background picture-uri ""
+gsettings set org.gnome.desktop.background picture-uri ""
 
 # set background black color
 gsettings set org.gnome.desktop.background primary-color '#000000'
