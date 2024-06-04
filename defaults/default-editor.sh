b85c6f0efab5c5f85dfce2ab09c8d079caa1ff52
yfedoruk <yfedoruck@gmail.com>
2024-06-04 14:27:28 +0300

    --

--- defaults/default-editor.sh
+++ defaults/default-editor.sh
@@ -3,5 +3,6 @@ https://askubuntu.com/questions/13447/how-do-i-change-the-default-text-editor#co
 # GUI editor
 sudo update-alternatives --install /usr/bin/gnome-text-editor gnome-text-editor $(which emacs) 100
 
-# terminal editor
-sudo update-alternatives --install /usr/bin/editor editor $(which nvim) 100
+# terminal editor (issues from snap package)
+# https://askubuntu.com/questions/1304472/update-alternatives-list-editor-does-not-show-editors-installed-via-snap#comment2653802_1304473
+sudo update-alternatives --install /usr/bin/editor editor /snap/nvim/current/usr/bin/nvim 111
