be5d8590d466a250f6a28d4020357c2d5c0c9d41
yfedoruk <yfedoruck@gmail.com>
2024-06-04 14:04:06 +0300

    --

--- defaults/default-editor.sh
+++ defaults/default-editor.sh
@@ -1,3 +1,5 @@
+https://askubuntu.com/questions/13447/how-do-i-change-the-default-text-editor#comment2397902_537762
+
 # GUI editor
 sudo update-alternatives --install /usr/bin/gnome-text-editor gnome-text-editor $(which emacs) 100
 
